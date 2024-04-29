74ecc3eb4cb4922d31666e8639fcffd8aa4380a8
Morgan Brooks <morgancassels@gmail.com>
2024-04-28 18:31:22 -0700

    attempt to reduce bundle size

--- personal-site/src/App.tsx
+++ personal-site/src/App.tsx
@@ -1,8 +1,6 @@
-import { Button, Card, Layout } from 'antd'
+import Layout, { Header } from 'antd/es/layout/Layout';
 import './App.css'
-import { Content, Header } from 'antd/es/layout/layout'
-import Link from 'antd/es/typography/Link';
-import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+import SiteContent from './SiteContent';
 
 function App() {
   return (
@@ -13,20 +11,7 @@ function App() {
           <p className="text-lg pt-4">Software Engineer</p>
         </div>
       </Header>
-      <Content className="bg-white space-y-6 px-[10vw] pt-6">
-        <Button
-          href="https://github.com/mcassels"
-          target="_blank"
-        >
-          <div><GithubOutlined className="pr-2" />Github</div>
-        </Button>
-        <Card className="max-w-fit" title="Get in touch">
-          <ul>
-            <li key="email">Email me: <Link href="mailto:morgan@morganbrooks.io" target="_blank">morgan@morganbrooks.io</Link></li>
-            <li>Say hi on <Link href="https://www.linkedin.com/in/morganjbrooks/" target="_blank">LinkedIn <LinkedinOutlined /></Link></li>
-          </ul>
-        </Card>
-      </Content>
+      <SiteContent />
     </Layout>
   );
 }
---
+++ personal-site/src/SiteContent.tsx
@@ -0,0 +1,25 @@
+import Button from 'antd/es/button/button';
+import Card from 'antd/es/card/Card';
+import './App.css'
+import { Content } from 'antd/es/layout/Layout';
+import Link from 'antd/es/typography/Link';
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+
+export default function SiteContent() {
+  return (
+    <Content className="bg-white space-y-6 px-[10vw] pt-6">
+      <Button
+        href="https://github.com/mcassels"
+        target="_blank"
+      >
+        <div><GithubOutlined className="pr-2" />Github</div>
+      </Button>
+      <Card className="max-w-fit" title="Get in touch">
+        <ul>
+          <li key="email">Email me: <Link href="mailto:morgan@morganbrooks.io" target="_blank">morgan@morganbrooks.io</Link></li>
+          <li>Say hi on <Link href="https://www.linkedin.com/in/morganjbrooks/" target="_blank">LinkedIn <LinkedinOutlined /></Link></li>
+        </ul>
+      </Card>
+    </Content>
+  );
+}
