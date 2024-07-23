75a9671ea6a7de5c6119fdbe108753d63ea61101
Morgan Brooks <morgancassels@gmail.com>
2024-07-23 08:29:21 -0700

    add resume to personal site

--- personal-site/src/SiteContent.tsx
+++ personal-site/src/SiteContent.tsx
@@ -2,17 +2,25 @@ import Button from 'antd/es/button/button';
 import Card from 'antd/es/card/Card';
 import './App.css'
 import Link from 'antd/es/typography/Link';
-import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+import { GithubOutlined, LinkedinOutlined, FileTextOutlined } from '@ant-design/icons';
 
 export default function SiteContent() {
   return (
     <div className="space-y-6 px-[10vw] pt-6">
-      <Button
-        href="https://github.com/mcassels"
-        target="_blank"
-      >
-        <div><GithubOutlined className="pr-2" />Github</div>
-      </Button>
+      <div className="space-x-6">
+        <Button
+          href="https://github.com/mcassels"
+          target="_blank"
+        >
+          <div><GithubOutlined className="pr-2" />Github</div>
+        </Button>
+        <Button
+          href="/morgan_brooks_resume.pdf"
+          target="_blank"
+        >
+          <div><FileTextOutlined className="pr-2" />Resume</div>
+        </Button>
+      </div>
       <Card className="max-w-fit" title="Get in touch">
         <ul>
           <li key="email">Email me: <Link href="mailto:morgan@morganbrooks.io">morgan@morganbrooks.io</Link></li>
